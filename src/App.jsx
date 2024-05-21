3b40a2b5f4fcfd273509f0da7b3057d1592610b1
JinkyXiang <109024403+JinkyXiang@users.noreply.github.com>
2024-05-21 20:35:13 +1200

    add places

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
 
 import "./App.css";
 
@@ -12,6 +13,8 @@ function App() {
       <main>
         <Routes>
           <Route path="/" exact element={<Users />} />
+          <Route path="/:userId/places" element={<UserPlaces />} />
+
           <Route path="/places/new" element={<NewPlace />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
